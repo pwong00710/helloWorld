8704f1914bced6db63031243cececc4339b23d71
pwong00710 <pwong00710@gmail.com>
2017-10-10 23:31:31 +0800

    updates

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,14 @@
 
 public class HelloWorld {
+	private String message = "Hello World!";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println((new HelloWorld()).getMessage());
+	}
+	
+	public String getMessage() {
+		return message;
 	}
 
 }
---
+++ src/test/java/Test.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		String expected = "Hello World!";
+		String actual = new HelloWorld().getMessage();
+		Assert.assertEquals(expected, actual);
+	}
+}
